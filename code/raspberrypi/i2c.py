44f622c2ef256329384a3f53c3aace8e4da1bc50
Mathieu David <mathieudavid@mathieudavid.org>
2017-02-02 17:43:06 +0100

    Improve I2C abstraction

--- code/raspberrypi/i2c.py
+++ code/raspberrypi/i2c.py
@@ -1,3 +1,4 @@
+import unittest
 import smbus
 
 
@@ -15,8 +16,41 @@ class I2C:
             self.bus.write_byte(self.adress, data)
 
     def receive(self, num_bytes=1):
-        data = []
-        for _ in range(num_bytes):
-            data.append(self.bus.read_byte(self.adress))
+        if num_bytes == 1:
+            return self.bus.read_byte(self.adress)
+        else:
+            data = []
+            for _ in range(num_bytes):
+                data.append(self.bus.read_byte(self.adress))
 
-        return data
+            return data
+
+    def pack8(x, y):
+        # Use  bitwise AND to retain only the first 4 bits
+        # of x and y, shift the four bits from x to the left
+        # and do a bitwise OR with the four bits of y
+        # to get a 8 bit value of the form: xxxx yyyy
+        return (x & 0b1111) << 4 | (y & 0b1111)
+
+    def pack16(x, y):
+        # Use  bitwise AND to retain only the first 8 bits
+        # of x and y, shift the 8 bits from x to the left
+        # and do a bitwise OR with the 8 bits of y
+        # to get a 16 bit value of the form: xxxx xxxx yyyy yyyy
+        return (x & 0xFF) << 8 | (y & 0xFF)
+
+
+class TestPack(unittest.TestCase):
+
+    def test_pack8(self):
+        self.assertEqual(I2C.pack8(0b0011, 0b0010), 0b00110010)
+        self.assertEqual(I2C.pack8(0b11000011, 0b0010), 0b00110010)
+        self.assertEqual(I2C.pack8(0b1111, 0b0000), 0b11110000)
+
+    def test_pack16(self):
+        self.assertEqual(I2C.pack16(0xF3, 0xAB), 0xF3AB)
+        self.assertEqual(I2C.pack16(0x2, 0xFF), 0x02FF)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- code/raspberrypi/range_sensors.py
+++ code/raspberrypi/range_sensors.py
@@ -6,8 +6,26 @@ class RangeSensor(I2C):
 
     def __init__(self, adress):
         super(RangeSensor, self).__init__(adress)
+        self.n = self.get_number_of_sensors()
 
     def get_range(self, sensor):
-        self.send(sensor)
-        r = self.receive()
-        return r[0]
+        cmd = I2C.pack8(1, sensor)
+        self.send(cmd)
+        r1 = self.receive()
+        r2 = self.receive()
+        return I2C.pack16(r1, r2)
+
+    def get_ranges(self):
+        cmd = I2C.pack8(2, 0)
+        self.send(cmd)
+        return self.receive(2 * self.n)
+
+    def enumerate_sensors(self):
+        cmd = I2C.pack8(3, 0)
+        self.send(cmd)
+        return self.receive(self.n)
+
+    def get_number_of_sensors(self):
+        cmd = I2C.pack8(4, 0)
+        self.send(cmd)
+        return self.receive()
