b6b757ea14732e28865962b282b2e56126db5fec
Jonathan Petit <13026@student.ecam.be>
2017-03-22 17:15:50 +0100

    Setup basic function for i2c
    iterface i2c lidar

--- code/arduino/lidar/lidar.ino
+++ code/arduino/lidar/lidar.ino
@@ -1,13 +1,26 @@
+#include <Wire.h>
 #include "lidar.h"
+
+const byte SLAVE_ADDRESS = 0x07;
 
 Lidar lidar();
 
 void setup() {
     Serial.begin(9600);
+
+    Wire.begin(SLAVE_ADDRESS);
+    Wire.onReceive(receive_i2c_data);
+    Wire.onRequest(send_i2c_data);
 }
 
 void loop() {
     analogRead(lidar.get_pin());
     lidar.get_distance_cm(measured_value);
     delay(60)
+}
+
+void receive_i2c_data(int byteCount) {
+}
+
+void send_i2c_data() {
 }
