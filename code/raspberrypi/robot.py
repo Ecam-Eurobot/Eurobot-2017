3865f81081455cfc3008d73e27cb6e37978bf339
Sylvain <alonso.sylvain@outlook.com>
2017-04-07 13:56:26 +0200

    Modifications robot.py

--- code/raspberrypi/robot.py
+++ code/raspberrypi/robot.py
@@ -4,40 +4,34 @@ from i2c import I2C
 import time
 
 class Robot():
-    i=0
     def __init__(self, arg):
-        self._arduinoservo = ArduinoServo(1)
+        self._kinematic = ArduinoServo(1)
         self._motors = Motors(5)
-        self._DELAY_OC_CLAMP = 0.7
-        self._DELAY_UD_CLAMP = 2.5
-        self._DELAY_SR_BLOCK = 2
-        self._DELAY_IO_BLOCK = 3
+        self._DELAY_OPEN_ClOSE_CLAMP = 0.7
+        self._DELAY_UP_DOWN_CLAMP = 2.5
+        self._DELAY_IN_OUT_BLOCK = 3
 
 
     def take_modules(self, number=1, distance=0):
         for i in range(number):
-            self._arduinoservo.release_block()
-            self._arduinoservo.down_clamp()
-            time.sleep(DELAY_UD_CLAMP)
+            self._kinematic.release_block()
+            self._kinematic.down_clamp()
+            time.sleep(DELAY_UP_DOWN_CLAMP)
             self._motors.forward(distance)
             while not self._motors.is_done():
                 time.sleep(0.5)
-            self._arduinoservo.close_clamp()
-            time.sleep(DELAY_OC_CLAMP)
+            self._kinematic.close_clamp()
+            time.sleep(DELAY_OPEN_CLOSE_CLAMP)
             self._motors.backward(distance)
             while not self._motors.is_done():
                 time.sleep(0.5)
-            self._arduinoservo.up_clamp()
-            time.sleep(DELAY_UD_CLAMP)
-            self._arduinoservo.open_clamp()
+            self._kinematic.up_clamp()
+            time.sleep(DELAY_UP_DOWN_CLAMP)
+            self._kinematic.open_clamp()
 
     def eject_modules(self, number=1):
         for i in range(number):
-            self._arduinoservo.stack_block()
-            time.sleep(DELAY_SR_BLOCK)
-            self._arduinoservo.push_out()
-            time.sleep(DELAY_IO_BLOCK)
-            self._arduinoservo.push_back()
-            time.sleep(DELAY_IO_BLOCK)
-            self._arduinoservo.release_block
-            time.sleep(DELAY_SR_BLOCK)
+            self._kinematic.push_out()
+            time.sleep(DELAY_IN_OUT_BLOCK)
+            self._kinematic.push_back()
+            time.sleep(DELAY_IN_OUT_BLOCK)
