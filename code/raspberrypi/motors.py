e9ab8e95334585e32f45a3d358239b84fa229807
Charles Vandevoorde <charles.vandevoorde@hotmail.be>
2017-04-12 17:56:37 +0200

    handle path instruction and add done callback
    when the regulation is actually done.

--- code/raspberrypi/motors.py
+++ code/raspberrypi/motors.py
@@ -1,3 +1,5 @@
+import time
+
 from enum import IntEnum
 from i2c import I2C
 
@@ -12,12 +14,37 @@ class Command(IntEnum):
     GetDistanceDone = 7
     IsDone = 8
     IsStopped = 9
-    Resume = 10
+    Restart = 10
 
 
 class Motors(I2C):
+    ANGLE_CORRECTION = 107.5 / 360
+
     def __init__(self, address):
         super(Motors, self).__init__(address)
+
+    def move_with_instructions(self, path, move_callback, done_callback):
+        for action in path:
+            val = action['value']
+            if action['action'] == 'move':
+                if val > 0:
+                    self.forward(val)
+                else:
+                    self.backward(val)
+            elif action['action'] == 'turn':
+                if val > 0:
+                    self.turn_right(val)
+                else:
+                    self.turn_left(val)
+
+            # Wait before action is done.
+            while not self.is_done(done_callback):
+                if move_callback() == 'obstacle':
+                    done_callback(self.get_distance_travelled(), 'obstacle')
+                    return
+                time.sleep(0.1)
+
+        done_callback(self.get_distance_travelled())
 
     def forward(self, distance):
         self.send([Command.Forward, distance])
@@ -37,7 +64,7 @@ class Motors(I2C):
     def stop(self):
         self.send(Command.Stop)
 
-    def distance_travelled(self):
+    def get_distance_travelled(self):
         self.send(Command.DistanceTravelled)
         r = self.receive(2)
         return {
@@ -45,13 +72,16 @@ class Motors(I2C):
             "right": r[1]
         }
 
-    def is_done(self):
+    def is_done(self, callback=None):
         self.send(Command.IsDone)
-        return self.receive()
+        is_done = self.receive()
+        if is_done and callback is not None:
+            callback(self.get_distance_travelled())
+        return is_done
 
     def is_stopped(self):
         self.send(Command.IsStopped)
         return self.receive()
 
-    def resume(self):
-        self.send(Command.Resume)
+    def restart(self):
+        self.send(Command.Restart)
